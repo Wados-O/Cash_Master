de5aa405fd6453dccacd6abf1c17e9aad0a3fb44
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-08-31 01:16:23 +0200

    feat : created readFile

--- src/CashMaster/util/FileUtil.java
+++ src/CashMaster/util/FileUtil.java
@@ -1,5 +1,24 @@
 package CashMaster.util;
 
+import CashMaster.model.Record;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class FileUtil {
+  private static String saveFile = "res/records.csv";
+  private static List<Record> records = new ArrayList<>();
+  private static final String SEP = ";;";
+
+  public static void readFile()throws IOException {
+    File file = new File(saveFile);
+    if (!file.exists() || file.length() == 0){
+      return;
+    }
+    Scanner sc = new Scanner(file);
+    //TODO file reader
+  }
 
 }
