af856c514c38353027ae1a2d229983f9c24b57fb
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-09-01 22:27:52 +0200

    feat : created constructor Category , getters and setters

--- src/CashMaster/model/Category.java
+++ src/CashMaster/model/Category.java
@@ -1,5 +1,38 @@
 package CashMaster.model;
 
 public class Category {
+private int id ;
+private String title;
+private boolean allowed;
 
+  public Category(int id, String title, boolean allowed) {
+    this.id = id;
+    this.title = title;
+    this.allowed = allowed;
+  }
+
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public boolean isAllowed() {
+    return allowed;
+  }
+
+  public void setAllowed(boolean allowed) {
+    this.allowed = allowed;
+  }
 }
