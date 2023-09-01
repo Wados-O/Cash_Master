384bc46a4d5cf18d79a0b527ca074dea4ac1752d
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-09-02 01:57:52 +0200

    style : created MenuButton.java

---
+++ src/CashMaster/view/MenuButton.java
@@ -0,0 +1,13 @@
+package CashMaster.view;
+
+import static CashMaster.view.Colors.GREEN;
+import static CashMaster.view.Colors.GREEN_BACKGROUND;
+import static CashMaster.view.Colors.RESET;
+import static CashMaster.view.Colors.WHITE_BOLD_BRIGHT;
+
+public class MenuButton {
+  public static final String EXIT_BUTTON = ""
+      + GREEN + "SAVE AND OUT:    "
+      + GREEN_BACKGROUND + WHITE_BOLD_BRIGHT + "  6 - EXIT   " + RESET;
+
+}
