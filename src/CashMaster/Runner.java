d08078d11cc1b8f245e93fdc1d760031b93af8ea
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-09-02 12:01:35 +0200

    feat : add todo to menu

--- src/CashMaster/Runner.java
+++ src/CashMaster/Runner.java
@@ -1,11 +1,12 @@
 package CashMaster;
 
 import CashMaster.controller.MenuController;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Runner extends MenuController {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
 
     MenuController.mainMenu(new Scanner(System.in));
   }
--- src/CashMaster/controller/MenuController.java
+++ src/CashMaster/controller/MenuController.java
@@ -4,14 +4,16 @@ import CashMaster.model.Category;
 import CashMaster.model.Record;
 import CashMaster.service.RecordOperation;
 import CashMaster.service.RecordsService;
+import CashMaster.util.FileUtil;
 import CashMaster.util.InputUtil;
 import CashMaster.view.MenuButton;
+import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
 public class MenuController {
-
-  public static void mainMenu(Scanner sc) {
+ static List<Record> records = FileUtil.getRecords();
+  public static void mainMenu(Scanner sc) throws IOException {
     boolean isRun = true;
     while (true) {
       System.out.println(MenuButton.SHOW_FIRST_MENU);
@@ -22,12 +24,16 @@ public class MenuController {
           RecordOperation.createRecord(sc);
           break;
         case 2:
+          //todo change records
           break;
         case 3:
+          //todo delete records
           break;
         case 4:
+          //todo sort menu;
           break;
         case 5:
+          FileUtil.saveToFile(records);
           break;
       }
     }
