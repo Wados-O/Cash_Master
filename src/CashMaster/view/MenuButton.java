ae6cb42c1cd581eaa1b0362c3e09e8b59c029f77
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-09-02 23:53:21 +0200

    style :CREATED SHOW SORT MENU

--- src/CashMaster/view/MenuButton.java
+++ src/CashMaster/view/MenuButton.java
@@ -20,4 +20,19 @@ public class MenuButton {
       + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + "  4 - SORT MENU      " + RESET + "   "
       + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + "  5 -  SAVE CHANGE   " + RESET + "   ";
 
+  public static final String SHOW_SORT_MENU = ""
+      + YELLOW + "SORT MENU:   "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + "  1 - CATEGORY SORT  " + RESET + "   "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + "  2 - COMMENT SORT   " + RESET + "   "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + "  3 - AMOUNT  SORT   " + RESET + "   "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + "  4 - SORT BY DATE   " + RESET + "   "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + "  5 -  SAVE CHANGE   " + RESET + "   ";
+
+  public static final String SHOW_CHANGE_MENU = ""
+      + YELLOW + "SORT MENU:   "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + " 1 - CHANGE CATEGORY " + RESET + "   "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + " 2 - CHANGE  COMMENT " + RESET + "   "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + " 3 - CHANGE  AMOUNT  " + RESET + "   "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + " 4 - CHANGE DATE OF  " + RESET + "   "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + " 5 -  SAVE  CHANGE   " + RESET + "   ";
 }
