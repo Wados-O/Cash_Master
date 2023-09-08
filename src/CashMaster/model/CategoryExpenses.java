ed143f44ae6cb928d70a51930b289164e9e101fd
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-09-08 16:13:18 +0200

    feat : created enum CategoryExpenses template version category  and category to enum

---
+++ src/CashMaster/model/CategoryExpenses.java
@@ -0,0 +1,31 @@
+package CashMaster.model;
+
+public enum CategoryExpenses {
+  FOOD(1,"Food"),
+  GOODS(2, "Goods"),
+  CLOTHES(3, "Clothes"),
+  ENTERTAINMENT(4, "Entertainment"),
+  HOUSE(5, "House"),
+  TRANSPORT(6, "Transport"),
+  FUEL(7, "Fuel"),
+  HOBBY(8, "Hobby"),
+  VACATIONS(9, "Vacations"),
+  MEDICINE(10, "Medicine");
+
+
+  private final int num;
+  private String title;
+
+  CategoryExpenses(int num, String title) {
+    this.num = num;
+    this.title = title;
+  }
+
+  public int getNum() {
+    return num;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+}
