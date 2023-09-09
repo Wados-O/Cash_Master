be4fe196f5ade6c456c2bd3c07a3c28fcbf5744f
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-09-09 22:36:08 +0200

    feat : added test readFile

--- tests/FileUtilTest.java
+++ tests/FileUtilTest.java
@@ -1,28 +1,61 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import CashMaster.model.Record;
+import CashMaster.util.FileUtil;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class FileUtilTest {
+private static final String testFile = "res/testRecord.csv";
+  @BeforeEach
+  void setUp(){
+    File file = new File(testFile);
+    if (file.exists()){
+      file.delete();
+    }
 
-  @org.junit.jupiter.api.BeforeEach
-  void setUp() {
   }
 
-  @org.junit.jupiter.api.AfterEach
-  void tearDown() {
+  @Test
+  void readFile() throws IOException , ParseException {
+  String testCsvData =  "1;;;Food;;;Lunch;;;15.50;;;01.01.2023\n" +
+      "2;;;Transportation;;;Bus Fare;;;20.00;;;02.01.2023";
+    try(BufferedWriter writer = new BufferedWriter(new FileWriter(testFile))) {
+      writer.write(testCsvData);
+    }
+    FileUtil.setSaveFile(testFile);
+    FileUtil.readFile();
+
+    List<Record> records = FileUtil.getRecords();
+    assertEquals(2,records.size());
+    Record firstRecord = records.get(0);
+    assertEquals(1,firstRecord.getId());
+    assertEquals("Food",firstRecord.getCategory());
+    assertEquals("Lunch",firstRecord.getComment());
+    assertEquals(15.50,firstRecord.getAmount(),0.01);
+    SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+    Date expectedDate = dateFormat.parse("01.01.2023");
+    assertEquals(expectedDate, firstRecord.getDate());
+
   }
 
-  @org.junit.jupiter.api.Test
-  void readFile() {
-  }
-
-  @org.junit.jupiter.api.Test
+  @Test
   void saveToFile() {
   }
 
-  @org.junit.jupiter.api.Test
+  @Test
   void setSaveFile() {
   }
 
-  @org.junit.jupiter.api.Test
+  @Test
   void getRecords() {
   }
 }
