bb7b8c3a0edd3953a7f6d282d13c367622b6932a
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-09-09 21:44:17 +0200

    feat : created package tests and test FileUtilTest.java

---
+++ tests/FileUtilTest.java
@@ -0,0 +1,28 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileUtilTest {
+
+  @org.junit.jupiter.api.BeforeEach
+  void setUp() {
+  }
+
+  @org.junit.jupiter.api.AfterEach
+  void tearDown() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void readFile() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void saveToFile() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void setSaveFile() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void getRecords() {
+  }
+}
