73c2bfc5bc1661e13c8b528a70e2e6f0ad650057
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-09-10 02:15:46 +0200

    feat : added test testGetDate

--- tests/RecordTest.java
+++ tests/RecordTest.java
@@ -1,57 +1,60 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import CashMaster.model.Record;
-import CashMaster.util.FileUtil;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.List;
-import java.util.Locale;
-import org.junit.jupiter.api.BeforeEach;
+
 import org.junit.jupiter.api.Test;
+
 class RecordTest {
+
   @Test
-  void testConstructor()throws ParseException{
+  void testConstructor() throws ParseException {
     Date date = new SimpleDateFormat("dd.MM.yyyy").parse("01.01.2023");
-    Record record = new Record(1,"Food","Lunch",15.50,date);
+    Record record = new Record(1, "Food", "Lunch", 15.50, date);
 
-    assertEquals(1,record.getId());
-    assertEquals("Food",record.getCategory());
-    assertEquals("Lunch",record.getComment());
-    assertEquals(15.50,record.getAmount());
-    assertEquals(date,record.getDate());
+    assertEquals(1, record.getId());
+    assertEquals("Food", record.getCategory());
+    assertEquals("Lunch", record.getComment());
+    assertEquals(15.50, record.getAmount());
+    assertEquals(date, record.getDate());
   }
 
 
   @Test
-  void testGetId(){
+  void testGetId() {
     Record record = new Record(1);
-    assertEquals(1,record.getId());
+    assertEquals(1, record.getId());
   }
+
   @Test
-  void testGetCategory(){
+  void testGetCategory() {
     Record record = new Record();
     record.setCategory("Food");
-    assertEquals("Food",record.getCategory());
+    assertEquals("Food", record.getCategory());
   }
+
   @Test
-  void testGetComment(){
+  void testGetComment() {
     Record record = new Record();
     record.setComment("Lunch");
-    assertEquals("Lunch",record.getComment());
+    assertEquals("Lunch", record.getComment());
   }
+
   @Test
-  void testGetAmount(){
+  void testGetAmount() {
     Record record = new Record();
     record.setAmount(15.50);
-    assertEquals(15.50,record.getAmount());
+    assertEquals(15.50, record.getAmount());
+  }
+
+  @Test
+  void testGetDate() throws ParseException {
+    Date date = new SimpleDateFormat("dd.MM.yyyy").parse("01.01.2023");
+    Record record = new Record();
+    record.setDate(date);
+    assertEquals(date, record.getDate());
   }
 
 }
