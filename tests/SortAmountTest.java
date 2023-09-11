e8541a9e9c12c835bf688d468b37af4c3270026d
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-09-11 14:59:57 +0200

    feat : added test testEmptyFile

--- tests/SortAmountTest.java
+++ tests/SortAmountTest.java
@@ -1,2 +1,29 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import CashMaster.Sorts.SortAmount;
+import CashMaster.model.Record;
+import CashMaster.util.FileUtil;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class SortAmountTest {
+    @Test
+    void testEmptyFile(){
+        List<Record> records = new ArrayList<>();
+        SortAmount.sortByAmount(records);
+        assertEquals(0,records.size());
+    }
+
 }
