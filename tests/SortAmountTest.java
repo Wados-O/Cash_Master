0b9d947b6b90e82036ad4e1dd562b39d20ba9879
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-09-17 09:09:02 +0200

    fix : deleted imports

--- tests/SortAmountTest.java
+++ tests/SortAmountTest.java
@@ -2,20 +2,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import CashMaster.Sorts.SortAmount;
 import CashMaster.model.Record;
-import CashMaster.util.FileUtil;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SortAmountTest {
