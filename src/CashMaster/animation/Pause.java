805c0634ebb196d52fc15c785eb8748863fc4986
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-09-18 08:14:19 +0200

    feat : added middle pause

--- src/CashMaster/animation/Pause.java
+++ src/CashMaster/animation/Pause.java
@@ -6,4 +6,9 @@ public class Pause {
         long start = System.currentTimeMillis();
         while (start >= System.currentTimeMillis() - pause);
     }
+    public static void middlePause(){
+        int pause =500;
+        long start = System.currentTimeMillis();
+        while (start >= System.currentTimeMillis() - pause);
+    }
 }
