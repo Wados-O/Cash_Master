530a0d82cc744f1c901a2a129406aa2319575e8a
Vadim Mejinschii <mezinskijvadim@gmail.com>
2023-11-18 11:55:03 -0500

    TEST COMMIT

--- src/CashMaster/view/MenuButton.java
+++ src/CashMaster/view/MenuButton.java
@@ -36,6 +36,12 @@ public class MenuButton {
           + WHITE_BOLD_BRIGHT + CYAN_BACKGROUND + "  1  -  INCOME      "  + RESET + "     "
           + WHITE_BOLD_BRIGHT + PURPLE_BACKGROUND + "  2  -  EXPENSE     " + RESET + "     ";
 
+  public static final String SHOW_BUTTON_OFF =  YELLOW + "CHANGE MENU:  "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + " 1 - CHANGE CATEGORY " + RESET + "     "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + " 2 - CHANGE  COMMENT " + RESET + "     "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + " 3 - CHANGE  AMOUNT  " + RESET + "     "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + " 4 - CHANGE DATE OF  " + RESET + "     "
+      + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + " 5 -  SAVE  CHANGE   " + RESET + "     ";
   public static final String SHOW_CHANGE_MENU = ""
       + YELLOW + "CHANGE MENU:  "
       + WHITE_BOLD_BRIGHT + YELLOW_BACKGROUND + " 1 - CHANGE CATEGORY " + RESET + "     "
